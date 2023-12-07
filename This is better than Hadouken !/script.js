1600a65a4c3211c264fba2e8bf1c265a871919b6
Kraosh <114225492+Kraosh@users.noreply.github.com>
2023-12-07 18:53:49 +0100

    dossier

---
+++ This is better than Hadouken !/script.js
@@ -0,0 +1,25 @@
+const konamiCode = ["ArrowUp", "ArrowUp", "ArrowDown", "ArrowDown", "ArrowLeft", "ArrowRight", "ArrowLeft", "ArrowRight", "b", "a"];
+
+let sequence = [];
+
+function detectKonamiCode(event) {
+    const key = event.key;
+        
+    console.log(key);
+    let i = sequence.length;
+    if (konamiCode[i] === key) {
+        
+        console.log(key);
+        sequence.push(key);
+    } else {
+        // Si l'utilisateur a entré une touche incorrecte, on réinitialise la séquence
+        sequence = [];
+    }
+
+    // Si la séquence est complète, on active le code Konami
+    if (sequence.length === konamiCode.length) {
+        console.log("coucou konami");
+    }
+}
+
+document.addEventListener("keydown", detectKonamiCode);
